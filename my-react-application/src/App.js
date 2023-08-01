6eca746ca772256ebc892cbf6cd5fb64e5edd45d
Shah Akshar <72061051+Shahakshar@users.noreply.github.com>
2023-08-01 19:01:25 +0000

    first changes in project

--- my-react-application/src/App.js
+++ my-react-application/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = () => {
+  return (
+    <>
+      <h1>First Name: Parth</h1>
+      <h2>Last Name: Patel</h2>
+      <h3>Age: 20</h3>
+    </>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person/>
     </div>
   );
 }
